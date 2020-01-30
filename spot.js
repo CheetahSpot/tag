bce13668b77be480fb5e4a01f50858f716e6f5a7
Paul Abrams <pabrams@gmail.com>
2020-01-29 23:24:32 -0800

    Update spot.js

--- spot.js
+++ spot.js
@@ -314,7 +314,7 @@ function SpotJs () {
     }
     let cookieVal2 = user[key];
     // Save the value as a cookie, but only if necessary
-    if (cookieVal2 !== undefined && cookieVal2 !== cookieVal && (cookieVal2 !== defaultVal && cookieVal === null)) {
+    if (cookieVal2 !== undefined && cookieVal2 !== cookieVal && (cookieVal2 !== defaultVal && !cookieVal)) {
       setCookie(key, cookieVal2);
     }
   }
